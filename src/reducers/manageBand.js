ca6d3707dd808cc7dd409485f2de31565f602ca2
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 06:23:56 -0500

    add src/manageBand.js to solution

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,8 +1,17 @@
-export default function manageBand(state = {bands: []}, action){
+let id = 0;
+
+export default function manageBand(state = {
+  bands: []
+}, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return Object.assign(state, {}, {bands: state.bands.concat(action.payload)})
+      id++
+      const band = Object.assign({}, action.band, { id: id })
+      return { bands: state.bands.concat(band) }
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
     default:
       return state;
   }
-}
+};
