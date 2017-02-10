1fc2609654b7630db4c6d6cd1e5d511ac1667c30
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 06:30:01 -0500

    remove use of Object.assign() as return for ADD_BAND action

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,7 +3,7 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return Object.assign({}, state, { bands: state.bands.concat(action.band) })
+      return { bands: state.bands.concat(action.band) }
     default:
       return state;
   }
