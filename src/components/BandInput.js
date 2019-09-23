173cf188b2fd6e5c8e33f6ddf5a9bf5608d8821a
Maxwell Benton <maxwellbenton@gmail.com>
2019-09-23 18:22:18 -0400

    fixes starter code typo

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -26,7 +26,7 @@ class BandInput extends Component {
         <form onSubmit={(event) => this.handleOnSubmit(event)}>
           <input
             type="text"
-            value={this.state.text}
+            value={this.state.bandName}
             onChange={(event) => this.handleOnChange(event)} />
           <input type="submit" />
         </form>
