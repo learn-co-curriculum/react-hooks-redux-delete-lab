87eb85339cf3ecc7b736275c1ad80a34fc64ff30
ihollander <ianholla@gmail.com>
2020-12-20 06:42:40 -0500

    gitignore

--- src/__test__/Band.test.js
+++ src/__test__/Band.test.js
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import React from "react";
+import { Provider } from "react-redux";
 import { fireEvent, render, screen } from "@testing-library/react";
 import Band from "../features/bands/Band";
 import { bandAdded } from "../features/bands/bandsSlice";
@@ -12,18 +13,30 @@ const { entities } = store.getState().bands;
 const [band1, band2] = entities;
 
 test("takes a prop of band and creates an element displaying the band name", () => {
-  render(<Band band={band1} />);
+  render(
+    <Provider store={store}>
+      <Band band={band1} />
+    </Provider>
+  );
   expect(screen.queryByText(band1.name)).toBeInTheDocument();
 });
 
 test("displays a delete button", () => {
-  render(<Band />);
+  render(
+    <Provider store={store}>
+      <Band band={band1} />
+    </Provider>
+  );
   expect(screen.queryByText(/Delete Band/i)).toBeInTheDocument();
   expect(screen.queryByText(/Delete Band/i).tagName).toBe("BUTTON");
 });
 
 test("clicking the delete button removes the band from the redux store", () => {
-  render(<Band band={band2} />);
+  render(
+    <Provider store={store}>
+      <Band band={band2} />
+    </Provider>
+  );
   fireEvent.click(screen.queryByText(/Delete Band/i));
 
   expect(store.getState().bands.entities).toEqual([band1]);
